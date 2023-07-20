51cc996252a0a662645eb00a998bf1452db84047
K-ima <bulgin.a.v@bk.ru>
2023-07-20 19:29:31 +0300

    Initial Commit

---
+++ Lesson 2.01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson 2.01
+//
+//  Created by Kima on 20.07.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
